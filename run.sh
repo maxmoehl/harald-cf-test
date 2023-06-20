e9d0c570bda8ffb5e1af04fb31eb806c918fe9db
Maximilian Moehl <maximilian@moehl.eu>
2023-06-20 14:35:50 +0200

    fix: try more often to shut down envoy

--- run.sh
+++ run.sh
@@ -4,7 +4,10 @@ set -exo pipefail
 
 admin_port="$(yq '.admin.address.socket_address.port_value' "/etc/cf-assets/envoy_config/envoy.yaml")"
 
-curl -ifX POST "127.0.0.1:${admin_port}/drain_listeners"
+# envoy takes a moment to come up
+curl -ifX POST \
+  --retry 10 --retry-all-errors --retry-delay 1 \
+  "127.0.0.1:${admin_port}/drain_listeners"
 
 envoy-to-harald "/etc/cf-assets/envoy_config/envoy.yaml" > "/etc/cf-assets/harald.yml"
 
